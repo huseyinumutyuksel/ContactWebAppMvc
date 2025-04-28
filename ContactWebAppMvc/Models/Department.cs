e1a5cf294f09ccb5fb1146831fb902e20a5d216f
huseyinumutyuksel <127051605+huseyinumutyuksel@users.noreply.github.com>
2025-04-28 23:07:19 +0300

    proje oluşturuldu

---
+++ ContactWebAppMvc/Models/Department.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace ContactWebAppMvc.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+
+        public string DepartmentName { get; set; }
+    }
+}
---
+++ ContactWebAppMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ContactWebAppMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
